8ee0690112963dcdbb1daf93b6b916d1435c0c7f
kerxl <63155572+kerxl@users.noreply.github.com>
2020-06-02 15:14:09 +0300

    temp | player Move

--- src/player/playerControllers/playerController.js
+++ src/player/playerControllers/playerController.js
@@ -1,11 +1,15 @@
 import { PlayerKeyController } from "./keyController";
+import { PlayerMoveController } from "./moveController";
 
 export class PlayerController {
-    constructor() {
+    constructor({ player, prop }) {
         this.keyController = new PlayerKeyController();
+        this.moveController = new PlayerMoveController(player, this.keyController, prop.moveController);
     }
 
     start() { this.keyController.start(); }
 
     stop() { this.keyController.stop(); }
+
+    update() { this.moveController.update(); }
 }
