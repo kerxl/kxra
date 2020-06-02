931814e655ce74d811a54198db025e7f88d2676f
kerxl <63155572+kerxl@users.noreply.github.com>
2020-06-02 15:15:44 +0300

    temp

--- src/player/player.js
+++ src/player/player.js
@@ -5,13 +5,13 @@ import { PlayerController } from "./playerControllers/playerController";
 export class Player {
     constructor(prop) {
         this.name = prop.name;
+        this.moveSpeed = prop.moveSpeed;
 
         this.view = new ViewBody(prop.view);
         this.body = new PlayerBody(prop.body);
-        
-        this.controller = new PlayerController();
 
-        this._isInitialized = false;
+        
+        this.controller = new PlayerController({ player: this, prop: prop.controller });
     }
 
     init() {
@@ -22,7 +22,11 @@ export class Player {
         this._isInitialized = true;
     }
 
-    update(time) { this.view.update(time); }
+    update(time) {
+        this.view.update(time);
+
+        this.controller.update();
+    }
 
     render(time, screen) {
         this.update(time);
