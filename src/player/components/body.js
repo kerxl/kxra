4a5256e52a7fbaab512659a5366ea509da39b204
kerxl <63155572+kerxl@users.noreply.github.com>
2020-06-02 18:41:41 +0300

    dv0.0.0 update | BoxCollider added BoxCollider class

--- src/player/components/body.js
+++ src/player/components/body.js
@@ -1,6 +1,10 @@
+import { BoxCollider } from "./boxCollider";
+
 export class PlayerBody {
     constructor(prop) {
         this.x = prop.x;
         this.y = prop.y;
+
+        this.boxCollider = new BoxCollider(prop.boxCollider);
     }
 }
---
+++ src/player/components/boxCollider.js
@@ -0,0 +1,9 @@
+export class BoxCollider {
+    constructor(prop) {
+        this.width = prop.width;
+        this.height = prop.height;
+
+        this.x = prop.x;
+        this.y = prop.y;
+    }
+}
